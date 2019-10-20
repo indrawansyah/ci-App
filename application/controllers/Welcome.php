c98c6a07f796b384e696d7d557b886ee6ca93542
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-20 15:27:48 +0700

    belajar sedikit menggenai codeigniter

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+
+	public function index()
+	{
+		echo 'hallo world';
+	}
+
+
+	public function about()
+	{
+		echo 'Hallo Indrawansyah';
+	}
+}
