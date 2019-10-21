7193a29eee0e096a672d58132d0953c731cee148
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-21 16:40:26 +0700

    membuat struktur dari peoples

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,4 +1,4 @@
 
-    <h1>Hello, <?= $nama; ?>! </h1>
+    <h1>Hello, <?= $nama; ?>Indrawansyah Prasetyo! </h1>
 
    
---
+++ application/views/peoples/index.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+
+            <h3 class="mt-3">List off peoples</h3>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+              <tbody>
+                  <tr>
+                      <th>1</th>
+                      <td>Indrawansyah</td>
+                      <td>Indrawansyah@mail.unpas.ac.id</td>
+                      <td>
+                          <a href="" class="badge badge-warning">detail</a>
+                          <a href="" class="badge badge-success">edit</a>
+                          <a href="" class="badge badge-danger">delete</a>
+                      </td>
+                  </tr>
+              </tbody>  
+            </table>
+
+        </div>
+    </div>
+</div>
